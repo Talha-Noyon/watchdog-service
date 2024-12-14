e4ae4065ab14c3904e7a095f6e4a7430146d7817
Talha-Noyon <talha.noyon10@gmail.com>
2024-12-15 00:00:04 +0600

    tuning CMakeLists config

--- build/CMakeFiles/WatchdogApp.dir/src/utils/math.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/WatchdogApp.dir/src/utils/math.cpp.obj: \
- C:\Users\Alvi\Documents\watchdog-service\src\utils\math.cpp \
- C:/Users/Alvi/Documents/watchdog-service/include/utils/math.h
--- build/CMakeFiles/WatchdogApp.dir/src/watchdog/linux_api.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/WatchdogApp.dir/src/watchdog/linux_api.cpp.obj: \
- C:\Users\Alvi\Documents\watchdog-service\src\watchdog\linux_api.cpp
